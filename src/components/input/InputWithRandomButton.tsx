96ff761ef651b1a663034af29b2628d5d010502b
허제인 <codingjamee@gmail.com>
2025-01-22 16:55:59 +0900

    #8 - input value 업데이트 설정

--- src/components/input/InputWithRandomButton.tsx
+++ src/components/input/InputWithRandomButton.tsx
@@ -1,27 +1,25 @@
 import Random from '../../assets/Random.svg?react';
+import { GameNameType } from '../../stores/gameStore';
+import { PlayerNamesType } from '../../stores/playerStore';
 interface Props {
   label: string;
   placeholder: string;
   onClickRandom: () => void;
   onChangeFn: (value: string) => void;
+  value: GameNameType | PlayerNamesType['name'];
 }
-const InputWithRandomButton = ({ label, placeholder, onClickRandom, onChangeFn }: Props) => {
+const InputWithRandomButton = ({ label, placeholder, onClickRandom, onChangeFn, value }: Props) => {
   return (
     <div className="input-container">
       <label htmlFor={label}>{label}</label>
       <div className="input-btn">
         <input
+          value={value}
           name={label}
           placeholder={placeholder}
           onChange={(e) => onChangeFn(e.target.value)}
         />
-        <button
-          className="btn btn-random"
-          onClick={() => {
-            console.log('random button clicked');
-            onClickRandom();
-          }}
-        >
+        <button className="btn btn-random" onClick={onClickRandom}>
           <Random />
         </button>
       </div>
